c900dc3e241daa4ed12a5362575b570de396424d
vishalgovil <vishalgovil5@gmail.com>
2023-10-29 20:02:57 +0530

    Added header and footer

--- megablog/src/App.js
+++ megablog/src/App.js
@@ -1,12 +1,29 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
-
+import authService from './appwrite/auth';
+import {login,logout} from './store/authSlice'
 function App() {
-  console.log(process.env.REACT_APP_APPWRITE_URL)
-  return (
-    <div className="App">
-      <h1>This is Appwrite application</h1>
-    </div>
-  );
+  // console.log(process.env.REACT_APP_APPWRITE_URL)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData)
+      {
+        dispatch(login({userData}))
+      }
+      else{
+dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  },[])
+
+  return !loading ? (<div><Header/>
+  {/* <main>Outlet</main> */}
+  <Footer/></div>) : null
 }
 
 export default App;
